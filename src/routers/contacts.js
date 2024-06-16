a53c1545412ec19c2a8ca0d8d791be1484add84a
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-06-16 07:57:14 +0300

    Deleted '/contacts' from router's path in src/routers/contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -18,29 +18,28 @@ const router = Router();
 
 //authenticate
 router.use(authenticate);
+
+//Route to get all contacts we (removed "/contacts")
 router.get('/', ctrlWrapper(getContactsController));
 
-//Route to get all contacts
-router.get('/contacts', ctrlWrapper(getContactsController));
+// Route to get a contact by ID (removed "/contacts")
+router.get('/:contactId', ctrlWrapper(getContactByIdController));
 
-// Route to get a contact by ID
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-
-// Route for create a contact
+// Route for create a contact (removed "/contacts")
 router.post(
-  '/contacts',
+  '/',
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
 
-// Route for partialy update (PATCH) a contact
+// Route for partialy update (PATCH) a contact (removed "/contacts")
 router.patch(
-  '/contacts/:contactId',
+  '/:contactId',
   validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
 
-// Route for delete a contact
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+// Route for delete a contact (removed "/contacts")
+router.delete('/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
